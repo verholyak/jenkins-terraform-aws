4856800e06227e36211a43aeecde59fecb71a61c
verholyak <61271519+verholyak@users.noreply.github.com>
2020-04-11 17:07:21 +0300

    Update user-data.sh
    add zabbix agent

--- user-data.sh
+++ user-data.sh
@@ -6,3 +6,16 @@ echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform using External Scr
 echo "<br><font color="blue">Hello World !" >> /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
+rpm -Uvh https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64/zabbix-release-4.4-1.el7.noarch.rpm
+yum -y install zabbix-agent
+cd /etc/zabbix/
+sed -i s/Server=127.0.0.1/Server=192.168.1.204/g zabbix_agentd.conf
+firewall-cmd --permanent --new-service=zabbix
+firewall-cmd --permanent --service=zabbix --add-port=10050/tcp
+firewall-cmd --permanent --service=zabbix --set-short="Zabbix Agent"
+firewall-cmd --permanent --add-service=zabbix
+firewall-cmd --reload
+iptables -I INPUT 1 -p tcp --dport 10050 -j ACCEPT
+service iptables save
+systemctl enable zabbix-agent
+systemctl start zabbix-agent
