c75b892bcf2327e8d037b282ba9e315a1d8aa157
verholyak <61271519+verholyak@users.noreply.github.com>
2020-04-12 22:30:04 +0300

    Update user-data.sh
    add new repo zabbix

--- user-data.sh
+++ user-data.sh
@@ -6,9 +6,10 @@ echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform using External Scr
 echo "<br><font color="blue">Hello World !" >> /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
-rpm -Uvh https://repo.zabbix.com/zabbix/4.4/rhel/7/x86_64/zabbix-release-4.4-1.el7.noarch.rpm
-yum -y install zabbix-agent
+rpm -Uvh https://repo.zabbix.com/zabbix/4.0/rhel/6/x86_64/zabbix-release-4.0-2.el6.noarch.rpm
+yum -y install zabbix zabbix-agent
 cd /etc/zabbix/
 sed -i s/Server=127.0.0.1/Server=192.168.1.204/g zabbix_agentd.conf
-systemctl enable zabbix-agent
-systemctl start zabbix-agent
+iptables -A INPUT -p tcp -s 192.168.1.204 --dport 10050 -m state --state NEW,ESTABLISHED -j ACCEPT
+service zabbix-agent start
+chkconfig zabbix-agent on
