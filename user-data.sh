7eb1fcc8f97087a090ec96a64fadcb31a3720865
verholyak <61271519+verholyak@users.noreply.github.com>
2020-04-13 13:47:47 +0300

    Update user-data.sh
    add ServerActive

--- user-data.sh
+++ user-data.sh
@@ -10,6 +10,8 @@ rpm -Uvh https://repo.zabbix.com/zabbix/4.0/rhel/6/x86_64/zabbix-release-4.0-2.e
 yum -y install zabbix zabbix-agent
 cd /etc/zabbix/
 sed -i s/Server=127.0.0.1/Server=192.168.1.204/g zabbix_agentd.conf
+sed -i s/ServerActive=127.0.0.1/ServerActive=192.168.1.204/g zabbix_agentd.conf
 iptables -A INPUT -p tcp -s 192.168.1.204 --dport 10050 -m state --state NEW,ESTABLISHED -j ACCEPT
 service zabbix-agent start
+service zabbix-agent restart
 chkconfig zabbix-agent on
