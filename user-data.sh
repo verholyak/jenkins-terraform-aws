a6890c663e82584f94d82b6cb506b676b9e89bf4
verholyak <61271519+verholyak@users.noreply.github.com>
2020-04-15 16:29:02 +0300

    Update user-data.sh

--- user-data.sh
+++ user-data.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 yum -y update
 yum -y install httpd
+yum -y install nmap
 myip=` curl http://169.254.169.254/latest/meta-data/local-ipv4`
-echo "<h2>WebServer with IP: $myip</h2><br>Build by Terraform using External Script !" > /var/www/html/index.html
+echo "<h2>WebServer with IP: $myip</h2><br>Build by Power of Terraform !" > /var/www/html/index.html
 echo "<br><font color="blue">Hello World !" >> /var/www/html/index.html
 sudo service httpd start
 chkconfig httpd on
